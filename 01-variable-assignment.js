ce8f9d92a4d0973201b7cd0d53e7d439270876ba
mahiera03 <mm3917416@gmail.com>
2024-07-31 07:53:33 +0530

    Javascript Variables
    Javascript Variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,5 @@
+console.log("My city name is:Pune");
+console.log("My Best Friend is:Mahesh");
+console.log("My country name is: INDIA");
+console.log("My School Name is: Madhyamik Vidyalaya Nigadi BK||");
+console.log("My College name is: FTC college of Engineering Sangola");
---
+++ 01-variable.js
@@ -0,0 +1,6 @@
+var firstName;
+console.log(firstName);
+firstName = "Mahadev";
+console.log(firstName);
+var lastName = "Erande";
+console.log(lastName);
