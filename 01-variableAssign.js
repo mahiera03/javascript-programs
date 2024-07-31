cb0ac036b3e19371bd68362739cc7d1bba8fc050
mahiera03 <mm3917416@gmail.com>
2024-07-31 10:31:25 +0530

    Assignments On Datatypes, Variable, TypeOf Operator
    Assignments On Datatypes, Variable, TypeOf Operator.

---
+++ 01-variableAssign.js
@@ -0,0 +1,39 @@
+// Variable Declaration
+var myFullName;
+//Printing the value of myFullName variable
+console.log(myFullName);
+
+//Variable Initialization
+myFullName = "Mahadev Erande";
+//Printing the value of myFullName variable
+console.log("My full Name is:", myFullName);
+
+//Updating the value of myFullName Variable
+myFullName = "Mahadev Malhari Erande";
+//Printing the updated value of myFullName variable
+console.log("My full Name is:", myFullName);
+
+//myLove variable dec &init .
+var myLove = "I love only JS";
+console.log(myLove);
+
+var firstName = "Mahadev";
+console.log("My first name is:", firstName);
+
+var lastName = "Erande";
+console.log("My last name is:", lastName);
+
+var age = 22;
+console.log("My age is:", age);
+
+var address = "Nigadi BK, Jath, Sangli";
+console.log("My permanent address is:", address);
+
+var pinCode = 416413;
+console.log("My pin code is:", pinCode);
+
+var gender = "Male";
+console.log("My gender is:", gender);
+
+var isMarried = "No";
+console.log("isMarried:", isMarried);
