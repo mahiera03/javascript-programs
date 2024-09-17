052874e56e0da62c54a0028a01cdf5e57fa13c6e
mahiera03 <mm3917416@gmail.com>
2024-09-17 21:11:09 +0530

    JavaScript Object Logical Programs(10).
    JavaScript Object Logical Programs(10).

---
+++ Practice.js
@@ -0,0 +1,16 @@
+var txt = "Hello, Good Morning, My Buddy";
+// console.log(txt.indexOf("Good"));
+
+console.log(txt.lastIndexOf("d"));
+console.log(txt.indexOf("B"));
+
+var str = "It may or may not be";
+
+var res = str.replaceAll("may", "can");
+console.log(res);
+
+var res = str.search("may");
+console.log(res);
+
+var str = "My name is Mahadev";
+console.log(str.includes("name"));
